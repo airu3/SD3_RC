80b2ec81a5b46a9c6e05585779fd488b961e7bbd
airu3 <sugar.tom5310@gmail.com>
2025-11-05 02:22:47 +0900

    デバッグモードのコメントアウト、ポート接続待機中の出力を整理し、LED制御コードを削除

--- SD3_rc_mini.ino
+++ SD3_rc_mini.ino
@@ -1,27 +1,20 @@
-#define DEBUG_MODE
+//#define DEBUG_MODE
+#define MINIZADE
 #include "SD3/ALL.h"
 
 void loop()
 {
-	SOUND(0);
-	
-	Serial.print(F("Please R/C system connect\n"));
-	while (!digitalRead(PB1))
-	{
-		print_port(0, 21);
 
-		static unsigned long time;
-		if (millis() - time > 1000)
-		{
-			SET_LED(1);
-			time = millis();
-		}
-		else
-		{
-			SET_LED(0);
-		}
+	//SOUND(0);
+
+	Serial.print(F("Please R/C system connect\n"));
+	while (port(BTN1))
+	{
+		update_port(STICK1);
+		update_port(STICK2);
+		print_port(0, 6);
 	}
-	SOUND(2);
+	SOUND(0);
 	Serial.print(F("Motor safety unlocked!!!\n"));
 	Drive();
 }
