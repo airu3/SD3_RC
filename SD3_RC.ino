4fadc68918620063c78bd9926ed0761f9781a38f
airu3 <sugar.tom5310@gmail.com>
2025-11-07 11:52:11 +0900

    SD3_RC.inoファイルを新規作成し、R/Cシステムの接続とモーターの安全解除機能を実装

---
+++ SD3_RC.ino
@@ -0,0 +1,27 @@
+#define DEBUG_MODE
+#include "SD3/ALL.h"
+
+void loop()
+{
+	SOUND(0);
+	
+	Serial.print(F("Please R/C system connect\n"));
+	while (!digitalRead(PB1))
+	{
+		print_port(0, 21);
+
+		static unsigned long time;
+		if (millis() - time > 1000)
+		{
+			SET_LED(1);
+			time = millis();
+		}
+		else
+		{
+			SET_LED(0);
+		}
+	}
+	SOUND(2);
+	Serial.print(F("Motor safety unlocked!!!\n"));
+	Drive();
+}
